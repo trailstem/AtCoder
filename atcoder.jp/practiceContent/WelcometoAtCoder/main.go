e0f1e70c090d3337bc49e549270406ece46e15a7
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-02-24 10:21:42 +0900

    提出(テスト)

--- atcoder.jp/practiceContent/WelcometoAtCoder/main.go
+++ atcoder.jp/practiceContent/WelcometoAtCoder/main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var f [3]int
+	var str string
+
+	fmt.Scan(&f[0], &f[1], &f[2], &str)
+
+	var result int
+	for i := 0; i < len(f); i++ {
+		result += f[i]
+	}
+
+	fmt.Println(result, str)
 }
