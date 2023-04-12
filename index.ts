b6456ea24865ce951634fd0880431fef4e97675b
github user tmcoco0228si <proguteinitar3150@gmail.com>
2023-04-12 23:28:41 +0900

    edit

--- index.ts
+++ index.ts
@@ -5,6 +5,7 @@ const targetArray = inputText.split("");
 
 let result = true;
 
+
 for (let i = 0; i < targetArray.length; i++) {
   let target = targetArray[i];
   let count = (inputText.match(new RegExp(target, "g")) || []).length;
